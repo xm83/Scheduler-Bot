764bf2fbee98063aaa52e09c2e0d2198b3753f2d
Sally Ma <sally.ma@yale.edu>
2018-07-24 18:39:33 -0700

    oauth success and basic integration with calendar

--- cal.js
+++ cal.js
@@ -1,33 +1,26 @@
 // models
-const User = require('./models').User;
+const User = require('./models').User
+const Task = require('./models').Task
 
 // google api setup
-const {google} = require('googleapis');
+const {google} = require('googleapis')
 
+// which apis i use
 const scopes = [
   'https://www.googleapis.com/auth/plus.me',
   'https://www.googleapis.com/auth/calendar'
-];
+]
 
-// new client
-const oauth2Client = new google.auth.OAuth2(
-  process.env.GOOGLE_CLIENT_ID,
-  process.env.GOOGLE_CLIENT_SECRET,
-  process.env.DOMAIN + '/google/callback'
-);
+async function addEvent (oauth2Client) {
+  // access calendar on behalf of this new client
+  const calendar = google.calendar({version: 'v3', auth: oauth2Client})
 
-// calendar
-const calendar = google.calendar({
-  version: 'v3',
-  auth: oauth2Client
-})
-
-
-async function addEvent() {
   return new Promise((resolve, reject) => {
     calendar.event.insert({
       calendarId: 'primary',
       summary: 'eat fish',
+      location: '800 Howard St., San Francisco, CA 94103',
+      'description': 'A chance to hear more about Google\'s developer products.',
       start: {
         date: new Date(Date.now() + 30000) // 30 seconds from now
       },
@@ -35,25 +28,111 @@ async function addEvent() {
         date: new Date(Date.now() + 90000)
       }
     }, (err, {data}) => {
-      if (err) return console.log("error:", error);
-      console.log(data);
-      resolve(resp);
+      if (err) {
+        console.log('The API returned an error: ' + err)
+        reject(err) // return the error object
+      } else {
+        console.log('success:', data)
+        resolve(data) // return the resulting event object
+      }
     })
   })
 }
 
+async function listEvents (oauth2Client) {
+  // access calendar on behalf of this new client
+  const calendar = google.calendar({version: 'v3', auth: oauth2Client})
 
-async function listEvents() {
-  return new Promise((resolve, reject) => {   
+  return new Promise((resolve, reject) => {
     calendar.events.list({
-      calendarId: 'primary',
-      timeMin:(new Date()).toISOString(),
+      calendarId: 'primary', // customizable
+      timeMin: (new Date()).toISOString(),
       maxResults: 10,
       singleEvents: true,
       orderBy: 'startTime'
-    }, (err, resp) => {
-      console.log(resp);
-    })   
+    }, (err, {data}) => {
+      if (err) {
+        console.log('The API returned an error: ' + err)
+        reject(err) // return the error object
+      } else {
+        const events = data.items
+        if (events.length) {
+          console.log('Upcoming 10 events:')
+          events.map((event, i) => {
+            const start = event.start.dateTime || event.start.date
+            console.log(`${start} - ${event.summary}`)
+          })
+          resolve(events) // return the array of events
+        } else {
+          console.log('No upcoming events found.')
+          resolve(events)
+        }
+      }
+    })
   })
 }
 
+function makeCalendarAPICall (tokens) {
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    process.env.DOMAIN + '/google/callback'
+  )
+
+  // talk to google api on their behalf
+  oauth2Client.setCredentials(tokens)
+
+  // google calls this when the current access token is expired,
+  // giving you a new refresh token
+  oauth2Client.on('tokens', (tokens) => {
+    if (tokens.refresh_token) {
+      // store the refresh_token in my database
+      User.findOne({
+        access_token: tokens.access_token
+      })
+        .exec()
+        .then((user) => {
+          if (!user) {
+            console.log('user not found')
+            res.status(500).send('database error')
+          } else {
+            user.refresh_token = tokens.refresh_token
+            user.save()
+          }
+        })
+        .catch((err) => {
+          console.log('errorrrr', err)
+        })
+    } else {
+      console.log('no refresh token', tokens)
+    }
+  })
+
+  const action = 'insert';
+
+  // do tasks accordingly
+  if (action === 'insert') {
+    console.log('action is insert')
+    addEvent(oauth2Client)
+      .then((event) => {
+        console.log('event added:', event)
+      })
+      .catch((err) => {
+        console.log('error:', err)
+      })
+    // get a list of upcoming events for time conflict checking
+    listEvents(oauth2Client)
+      .then((events) => {
+        console.log('Events', events)
+      })
+  } else if (action === 'delete') {
+    console.log('action is delete')
+  } else {
+    console.log('action is', action)
+  }
+}
+
+module.exports = {
+  scopes,
+  makeCalendarAPICall
+}
--- server.js
+++ server.js
@@ -1,128 +1,163 @@
-const { RTMClient, WebClient } = require('@slack/client');
-const teamId = 'sjs-2018';
-const token = process.env.BOT_USER_OAUTH_ACCESS_TOKEN;
-let axios = require('axios');
-const express = require('express');
-const bodyParser = require('body-parser');
-const dialogflow = require('dialogflow');
+const { RTMClient, WebClient } = require('@slack/client')
+const teamId = 'sjs-2018'
+const token = process.env.BOT_USER_OAUTH_ACCESS_TOKEN
+let axios = require('axios')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const dialogflow = require('dialogflow')
 
 // models
-const User = require('./models').User;
+const User = require('./models').User
 
-const scheduleBotChannel = 'DBWNA5TCN';
+const scheduleBotChannel = 'DBWNA5TCN'
 
-// google api setup
-const {google} = require('googleapis');
-const {oauthClient2} = require('./cal');
+// gCal api setup
+const {google} = require('googleapis')
+const {scopes, makeCalendarAPICall} = require('./cal')
+// TODO: how to create a new unique google profile for every new user???
+const oauth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.DOMAIN + '/google/callback'
+)
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
+const rtm = new RTMClient(token)
+rtm.start()
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-const rtm = new RTMClient(token);
-rtm.start();
-
-rtm.on('message', event=> {
-  let message = event.text;
-  let channel = event.channel;
-  console.log(message, channel);
-  let responseMessage = 'sample response from backend';
-  const projectId = process.env.DIALOGFLOW_PROJECT_ID; //https://dialogflow.com/docs/agents#settings
-  const sessionId = 'quickstart-session-id';
-  const sessionClient = new dialogflow.SessionsClient();
-  const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+rtm.on('message', event => {
+  let message = event.text
+  let channel = event.channel
+  console.log(message, channel)
+  let responseMessage = 'sample response from backend'
+  const projectId = process.env.DIALOGFLOW_PROJECT_ID // https://dialogflow.com/docs/agents#settings
+  const sessionId = 'quickstart-session-id'
+  const sessionClient = new dialogflow.SessionsClient()
+  const sessionPath = sessionClient.sessionPath(projectId, sessionId)
   const request = {
     session: sessionPath,
     queryInput: {
       text: {
         text: message,
-        languageCode: 'en-US',
-      },
-    },
-  };
+        languageCode: 'en-US'
+      }
+    }
+  }
 
   sessionClient
     .detectIntent(request)
     .then(responses => {
-      console.log('Detected intent');
-      const result = responses[0].queryResult;
-      console.log(`  Query: ${result.queryText}`);
-      console.log(`  Response: ${result.fulfillmentText}`);
-      console.log(result.parameters);
+      console.log('Detected intent')
+      const result = responses[0].queryResult
+      console.log(`  Query: ${result.queryText}`)
+      console.log(`  Response: ${result.fulfillmentText}`)
+      console.log(result.parameters)
       rtm.sendMessage(result.fulfillmentText, channel)
       if (result.intent) {
-        console.log(`  Intent: ${result.intent.displayName}`);
+        console.log(`  Intent: ${result.intent.displayName}`)
       } else {
-        console.log(`  No intent matched.`);
+        console.log(`  No intent matched.`)
       }
     })
-  .then(msg =>
-    console.log('message sent:' + msg)
-  ).catch(err=> console.log("error", err));
-
+    .then(msg =>
+      console.log('message sent:' + msg)
+    ).catch(err => console.log('error', err))
 })
 
 // GET route that redirects to google oatuh2 url
-app.get('/google/calendar', function(req, res) {
-    var auth_id = req.query.auth_id;
-    // create a new user in database based on auth_id
-    var newUser = new User({auth_id});
-    newUser.save()
-    .then(() => {
-      // generate a url that asks permissions for Google+ and Google Calendar scopes
-      var url = oauth2Client.generateAuthUrl({
-        // 'online' (default) or 'offline' (gets refresh_token)
-        access_type: 'online',
-        // refresh_token only returned on the first authorization
+app.get('/google/calendar', function (req, res) {
+  // TODO: get slackId, task, and action from slack
+  let slackId = req.query.slackId || 'myslackId'
 
-        scope: scopes,
-        state: encodeURIComponent(JSON.stringify({
-          auth_id: auth_id
-        })),
-        prompt: 'consent'
-      });
-      res.redirect(url);
+  // save action into database?
+
+  // check if user exists
+  User.findOne({
+    slackId: slackId
+  })
+    .exec()
+    .then((user) => {
+      if (!user) {
+      // create a new user in database
+        var newUser = new User({
+          slackId: slackId
+        })
+        newUser.save()
+          .then((user) => {
+            // generate a url that asks permissions for Google+ and Google Calendar scopes
+            var url = oauth2Client.generateAuthUrl({
+            // 'online' (default) or 'offline' (gets refresh_token)
+              access_type: 'offline',
+              // refresh_token only returned on the first authorization
+              scope: scopes,
+              state: encodeURIComponent(JSON.stringify({
+                auth_id: user._id
+              })),
+              prompt: 'consent'
+            })
+            res.redirect(url)
+          })
+          .catch((err) => {
+            console.log('errorrrr', err)
+            res.status(500).send('internal error')
+          })
+      } else {
+        // user already exists: send query to Api.ai
+        // TODO
+      }
     })
     .catch((err) => {
-      res.send("error", err);
+      console.log('error finding user', err)
+      res.status(500).send('internal server error')
     })
 })
 
 // GET route that handles oauth callback for google api
-app.get('/google/callback', function(req, res) {
-    var code = req.query.code;
-    // This will provide an object with the access_token and refresh_token
-    oauth2Client.getToken(code, (err, tokens) => {
-      oauth2Client.setCredentials(tokens);
+app.get('/google/callback', function (req, res) {
+  var code = req.query.code
+  // This will provide an object with the access_token and refresh_token
+  oauth2Client.getToken(code, (err, tokens) => {
+    console.log('token!!', tokens)
+    if (err) return console.log('!!error:', err)
+    oauth2Client.setCredentials(tokens)
 
-      // look for user based on auth_id and store token in database
-      var auth_id = JSON.parse(decodeURIComponent(req,query.state))
-      User.findOne({auth_id})
+    // look for user based on auth_id and store token in database
+    var auth_id = JSON.parse(decodeURIComponent(req.query.state)).auth_id
+    console.log('auth_id!!!', auth_id)
+    User.findById(auth_id)
       .exec()
       .then((user) => {
-        // store token
-        access_token: tokens.access_token,
-        refresh_token: tokens.refresh_token,
-        expiry_date: tokens.expiry_date
+        if (!user) {
+          console.log('user not found')
+          res.status(500).send('database error')
+        } else {
+          user.access_token = tokens.access_token
+          user.refresh_token = tokens.refresh_token
+          user.save()
+          
+          // once you get the token, make API call
+          makeCalendarAPICall(tokens);
+          res.status(200).send('success')
+        }
       })
       .catch((err) => {
-        res.send("error", err);
+        console.log('errorrrr', err)
+        res.status(500).send('internal error')
       })
-
   })
-
 })
 
-
-app.post('/test', function(req, res){
-  console.log('something');
+app.post('/test', function (req, res) {
+  console.log('something')
   // res.setStatus(200);
   // res.setContentType('/text/plain');
   // res.getStreamWriter.writeString(req.body.data.challenge);
   // console.log(req);
-  res.send(req.body.challenge);
+  res.send(req.body.challenge)
   // res.send(req.body.data.challenge);
 })
 
-app.listen(1337);
+app.listen(1337)
